48300c2ffb33183ffe4990c33f0579a763add483
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-05-10 22:55:23 +0300

    add everything

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function syncDotfiles() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	syncDotfiles;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		syncDotfiles;
+	fi;
+fi;
+unset syncDotfiles;
